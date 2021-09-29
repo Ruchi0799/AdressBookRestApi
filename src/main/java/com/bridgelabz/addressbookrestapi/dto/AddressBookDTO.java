7e53d516659c9bd4e3ba4e7fd1fb496180668bba
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-29 19:43:38 +0530

    [ruchi]: implemented added constrains for other feilds in addressbook Spring boot

--- src/main/java/com/bridgelabz/addressbookrestapi/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookrestapi/dto/AddressBookDTO.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbookrestapi.dto;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import lombok.ToString;
@@ -14,13 +15,16 @@ public @ToString class AddressBookDTO {
 	@Pattern(regexp="(0/91)?[7-9][0-9]{9}",message="Phone No Invalid")
 	public String phoneNo;
 	
-	
+	@NotNull(message="address cannot be empty")
 	public String address;
 	
+	@NotNull(message="city cannot be empty")
 	public String city;
 	
+	@NotNull(message="state cannot be empty")
 	public String state;
 	
+	@NotNull(message="departments cannot be empty")
 	public List<String> departments;
 
 	
